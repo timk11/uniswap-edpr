3af6b1f03ffb015b528be3c88c90dbde66f98420
timk11 <timk@email.com>
2023-12-13 00:46:31 +1100

    updated algorithm lens logic

--- src/logics/usdceth_0_3_algorithm_lens/src/lib.rs
+++ src/logics/usdceth_0_3_algorithm_lens/src/lib.rs
@@ -30,34 +30,33 @@ pub async fn calculate (targets : Vec < String >) -> LensValue {
     
     let fees_24h_usd = pool_fees_result.unwrap().result[0].fees_24h_usd;
 
-    let t0_price_usd  = pool_usd_prices_result.unwrap().result.t0_price_usd;
-    let t1_price_usd  = pool_usd_prices_result.unwrap().result.t1_price_usd;
+    let t0_price_usd  = pool_usd_prices_result.clone().unwrap().result.t0_price_usd;
+    let t1_price_usd  = pool_usd_prices_result.clone().unwrap().result.t1_price_usd;
 
-    let address = v3pool_result.unwrap().result.address;
-    let current_tick_liquidity = v3pool_result.unwrap().result.liquidity;
-    let sqrt_ratio_x96 = v3pool_result.unwrap().result.sqrt_ratio_x96;
-    let tick_current = v3pool_result.unwrap().result.tick_current;
-    let tick_spacing = v3pool_result.unwrap().result.tick_spacing;
-    let ticks_str = v3pool_result.unwrap().result.ticks;
+    let address = v3pool_result.clone().unwrap().result.address;
+    let current_tick_liquidity = v3pool_result.clone().unwrap().result.liquidity;
+    let sqrt_ratio_x96 = v3pool_result.clone().unwrap().result.sqrt_ratio_x96;
+    let tick_current = v3pool_result.clone().unwrap().result.tick_current;
+    let tick_spacing = v3pool_result.clone().unwrap().result.tick_spacing;
+    let ticks_str = v3pool_result.clone().unwrap().result.ticks;
     let ticks = serde_json::from_str::<HashMap<String, Value>>(&ticks_str);
 
-    let tick_cumul_28x6h = tc28x6_result.unwrap()[0];
-    let tick_cumul_30m = tc30_result.unwrap()[0];
+    let tick_cumul_28x6h = tc28x6_result.unwrap().0;
+    let tick_cumul_30m = tc30_result.unwrap().0;
 
     let price_x96 = u128::pow(sqrt_ratio_x96.into(), 2) as f32;
     let mut current_price = price_x96 / f32::powf(2.0,192.0);
     let current_price = current_price / f32::powf(10.0,12.0);
 
-    let compressed: i32 = tick_current / tick_spacing;
+    let mut compressed: i32 = tick_current / tick_spacing;
     if tick_current <0 && tick_current % tick_spacing != 0 {
         compressed -= 1;
     }
     let floor = compressed * tick_spacing;
     let mut min_tick = floor;
     let mut max_tick = floor + tick_spacing;
-    let mut i;
     for i in (1..29).rev() {
-        let new_tick = (tick_cumul_28x6h[i].parse::<i32>().unwrap() - tick_cumul_28x6h[i-1].parse::<i32>().unwrap()) / 21600;
+        let new_tick = (tick_cumul_28x6h[i] as i32 - tick_cumul_28x6h[i-1] as i32) / 21600;
         if new_tick < min_tick {
             min_tick = new_tick;
         } else if new_tick > max_tick {
@@ -70,27 +69,28 @@ pub async fn calculate (targets : Vec < String >) -> LensValue {
         sum_liquidity = current_tick_liquidity as i128;
     }
 
+    let ticks_clone = ticks.expect("reason").clone();
     for i in (floor + tick_spacing..max_tick).step_by(tick_spacing as usize) {
-        match std::panic::catch_unwind(|| {
-            let tick_val = ticks.expect("reason").get(&i.to_string());
-            let tick: Tick = serde_json::from_value(*tick_val.unwrap()).unwrap();
+        match std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| {
+            let tick_val = ticks_clone.get(&i.to_string());
+            let tick: Tick = serde_json::from_value(tick_val.unwrap().clone()).unwrap();
             let liquidity_net_ = tick.liquidity_net;
             state += liquidity_net_;
             sum_liquidity += state;
-        }) {
+        })) {
             Ok(_) => {}
             Err(_) => {}
         }
     }
     state = current_tick_liquidity as i128;
     for i in (floor + tick_spacing..min_tick).rev().step_by(tick_spacing as usize) {
-        match std::panic::catch_unwind(|| {
-            let tick_val = ticks.expect("reason").get(&i.to_string());
-            let tick: Tick = serde_json::from_value(*tick_val.unwrap()).unwrap();
+        match std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| {
+            let tick_val = ticks_clone.get(&i.to_string());
+            let tick: Tick = serde_json::from_value(tick_val.unwrap().clone()).unwrap();
             let liquidity_net_ = tick.liquidity_net;
             state -= liquidity_net_;
             sum_liquidity += state;
-        }) {
+        })) {
             Ok(_) => {}
             Err(_) => {}
         }
